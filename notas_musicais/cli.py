d3bc6ca207e64010950db8bc2c0ec382c9533c32
eotaldoleozin <leo-alves14@hotmail.com>
2023-04-04 13:42:08 -0300

    Implementação do comando de acorde no CLI

--- notas_musicais/cli.py
+++ notas_musicais/cli.py
@@ -3,20 +3,34 @@ from rich.console import Console
 from rich.table import Table
 from typer import Argument, Typer
 
-from notas_musicais.escalas import escala
+from notas_musicais.acordes import acorde as _acorde
+from notas_musicais.escalas import escala as _escala
 
 app = Typer()
 
 
 @app.command()
-def escalas(
+def escala(
     tonica: str = Argument('C', help='Tônica da escala'),
     tonalidade: str = Argument('maior', help='Tonalidade da escala'),
 ):
     console = Console()
     table = Table()
-    notas, graus = escala(tonica, tonalidade).values()
+    notas, graus = _escala(tonica, tonalidade).values()
     for grau in graus:
         table.add_column(grau)
     table.add_row(*notas)
+    console.print(table)
+
+
+@app.command()
+def acorde(
+    cifra: str = Argument('C', help='Cifra de um acorde'),
+):
+    console = Console()
+    table = Table()
+    notas_acorde, graus_acorde = _acorde(cifra).values()
+    for grau in graus_acorde:
+        table.add_column(grau)
+    table.add_row(*notas_acorde)
     console.print(table)
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,29 +1,46 @@
 from pytest import mark
 from typer.testing import CliRunner
 
-from notas_musicais.cli import app, escalas
+from notas_musicais.cli import app
 
 runner = CliRunner()
 
 
 def test_escala_cli_deve_retornar_0_ao_stdout():
-    result = runner.invoke(app)
+    result = runner.invoke(app, ['escala'])
     assert result.exit_code == 0
 
 
 @mark.parametrize('nota', 'C D E F G A B'.split())
 def test_escala_cli_deve_conter_as_notas_na_resposta(nota):
-    result = runner.invoke(app)
+    result = runner.invoke(app, ['escala'])
     assert nota in result.stdout
 
 
 @mark.parametrize('nota', 'D E F# G A B C#'.split())
 def test_escala_cli_deve_conter_as_notas_na_resposta_de_re(nota):
-    result = runner.invoke(app, ['D'])
+    result = runner.invoke(app, ['escala', 'D'])
     assert nota in result.stdout
 
 
 @mark.parametrize('grau', 'I II III IV V VI VII'.split())
 def test_escala_cli_deve_conter_todos_os_graus(grau):
-    result = runner.invoke(app, ['D'])
+    result = runner.invoke(app, ['escala', 'D'])
+    assert grau in result.stdout
+
+
+def test_acorde_cli_deve_retornar_0_ao_stdout():
+    result = runner.invoke(app, ['acorde'])
+    assert result.exit_code == 0
+
+
+@mark.parametrize('nota', 'C E G'.split())
+def test_acorde_cli_deve_conter_as_notas_na_resposta(nota):
+    result = runner.invoke(app, ['acorde'])
+    assert nota in result.stdout
+
+
+@mark.parametrize('grau', 'I III V'.split())
+def test_acorde_cli_deve_conter_todos_os_graus(grau):
+    result = runner.invoke(app, ['acorde', 'D'])
     assert grau in result.stdout
